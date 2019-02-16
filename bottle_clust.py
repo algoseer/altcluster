07ca85fa3058ab543649e56bfc754886a06a6304
Naveen <naveen@otic.wdi.disney.com>
2019-02-15 17:48:01 -0800

    checked in conv method using mnist

--- bottle_clust.py
+++ bottle_clust.py
@@ -1,17 +1,21 @@
 import sys
 import numpy as np
+from tqdm import tqdm
 
 #Load the data
 
-
+'''
 labs=[]
 feats=[]
 
-for line in file(sys.argv[1]):
-    line=line.rstrip().split(',')
+N=len(open(sys.argv[1]).readlines())
 
-    labs.append(line[-1])
-    feats.append(map(float,line[:-1]))
+with open(sys.argv[1]) as fin:
+	for line in tqdm(fin,total=N):
+		line=line.rstrip().split(',')
+		labs.append(line[0])
+		line=np.array(line[1:]).astype('float')
+		feats.append(np.reshape(line,(28,28,1))/255.)
 
 feats=np.array(feats)
 lablist, labs = np.unique(labs, return_inverse=True)
@@ -19,37 +23,50 @@ nb_classes = labs.max()+1
 labs = np.eye(nb_classes)[labs]
 
 
-print feats.shape, labs.shape, lablist
-N,dim= feats.shape
+print(feats.shape, labs.shape, lablist)
+N = feats.shape[0]
 idx=range(N)
 
-from random import shuffle
-shuffle(idx)
+from random import sample
+idx=sample(idx,(N-1))
 feats=feats[idx]
 labs=labs[idx]
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Conv2D, MaxPooling2D, UpSampling2D, GlobalAveragePooling2D, Reshape
 
 model = Sequential()
-model.add(Dense(64, input_shape=(dim,),activation='relu'))
-model.add(Dense(16, activation='relu'))
+
+inp_shape = (28,28,1)
+## Convolutional model
+model.add(Conv2D(64,3,padding='same',activation='relu',input_shape=inp_shape))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(128,3,padding='same',activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(GlobalAveragePooling2D())
+
 model.add(Dense(nb_classes, activation='softmax'))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(dim))
+
+model.add(Dense(49*128, activation='relu'))
+model.add(Reshape((7,7,128)))
+model.add(UpSampling2D(2))
+model.add(Conv2D(64,3,padding='same',activation='relu'))
+model.add(UpSampling2D(2))
+model.add(Conv2D(1,3,padding='same',activation='relu'))
+
 
 #model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
 #model.fit(feats, labs, epochs=20, batch_size=32, validation_split=0.2)
 
 model.compile(loss='mse',  optimizer='adam')
-model.fit(feats, feats, epochs=200, batch_size=32, validation_split=0.2,verbose=False)
+model.fit(feats, feats, epochs=20, batch_size=32, validation_split=0.2,verbose=True)
 mse= model.evaluate(feats,feats)
-print
-print "mse=",mse
+print()
+print("mse=",mse)
+
 
 from keras.models import Model
-bneck_model = Model(inputs=[model.input], outputs=[model.layers[2].output])
+bneck_model = Model(inputs=[model.input], outputs=[model.layers[5].output])
 bneck_labs = bneck_model.predict(feats)
 bneck_hard_labs=np.argmax(bneck_labs,axis=1)
 hard_labs = np.argmax(labs, axis=1)
@@ -57,17 +74,27 @@ hard_labs = np.argmax(labs, axis=1)
 from sklearn.metrics import normalized_mutual_info_score as nmi
 from sklearn.metrics import mutual_info_score as mi
 
-print "nmi=",nmi(hard_labs, bneck_hard_labs)
-print "mi=",mi(hard_labs, bneck_hard_labs)
+print("nmi=",nmi(hard_labs, bneck_hard_labs))
+print("mi=",mi(hard_labs, bneck_hard_labs))
 
-#Compute probabilistic mutual information
-J= np.dot(labs.T, bneck_labs)/N
-K= np.outer(J.sum(axis=0), J.sum(axis=1))
-H1= bneck_labs*np.log(bneck_labs+1e-16)
-H1 = -H1.sum()/N
-H2= labs*np.log(labs+1e-16)
-H2= -H2.sum()/N
+'''
+#TODO Do a tsne on the layer before softmax?
+from sklearn.manifold import TSNE
+import random
+tsne = TSNE(n_components=2, random_state=0)
 
-pmi = J*np.log(J/K)
-print "Prob MI=",pmi.sum()
-print H1, H2
+bneck_feats=Model(model.input,model.layers[4].output).predict(feats[::20])
+bneck_tsne = tsne.fit_transform(bneck_feats)
+labs_sub = labs[::20]
+
+import matplotlib
+matplotlib.use('Agg')
+from pylab import *
+
+scatter(bneck_tsne[:,0],bneck_tsne[:,1], color=cm.rainbow(labs_sub.argmax(axis=1)/10.))
+for i in range(bneck_tsne.shape[0]):
+	if random()<0.2:
+		text(bneck_tsne[i,0],bneck_tsne[i,1],str(np.argmax(labs_sub[i])))
+
+savefig('/tmp/fig.png')
+clf()
